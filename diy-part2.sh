93bffbcd6f3d9e49cf99726e144dfea12a80abf0
blue1800 <63365195@qq.com>
2026-07-25 14:57:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -101,10 +101,10 @@ CONFIG_PACKAGE_luci-theme-argon=y
 " >> .config
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.100.27/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.100.27/g' package/base-files/files/bin/config_generate
 
 # 修改默认子网掩码
-sed -i 's/255.255.255.0/255.255.255.0/g' package/base-files/files/bin/config_generate
+#sed -i 's/255.255.255.0/255.255.255.0/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
 #sed -i 's/luci-theme-openwrt-2020/luci-theme-alpha/g' feeds/luci/collections/luci/Makefile
