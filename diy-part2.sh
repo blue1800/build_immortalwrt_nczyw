e93ac67807816e7e36a0286fe1c27202d371681b
blue1800 <63365195@qq.com>
2026-08-08 16:38:57 +0800

    Update package management in diy-part2.sh
    Removed several luci applications and added new package clones.

--- diy-part2.sh
+++ diy-part2.sh
@@ -156,7 +156,32 @@ EOF
 #git clone -b v3.15.3 --depth=1 https://github.com/stackia/rtp2httpd.git package/luci-app-rtp2httpd
 rm -rf ./feeds/packages/net/rtp2httpd
 rm -rf ./feeds/luci/applications/luci-app-rtp2httpd
+rm -rf ./feeds/luci/applications/luci-app-homeproxy
+rm -rf ./feeds/luci/applications/luci-app-openclash
+rm -rf ./feeds/luci/applications/luci-app-passwall
+
 git clone -b v3.15.3 --depth=1 https://github.com/stackia/rtp2httpd.git package/luci-app-rtp2httpd
+
+git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
+
+git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall.git package/openwrt-passwall
+
+git clone --depth=1 https://github.com/vernesong/OpenClash.git package/openclash
+
+git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/openappfilter
+
+git clone --depth=1 https://github.com/sbwml/luci-app-quickfile.git package/quickfile
+
+git clone --depth=1 https://github.com/kiddin9/luci-theme-edge.git package/luci-theme-edge
+
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+
+git clone --depth=1 https://github.com/sirpdboy/luci-theme-kucat.git package/luci-theme-kucat
+
+git clone --depth=1 https://github.com/sirpdboy/luci-app-kucat-config.git package/luci-app-kucat-config
+
 
 # 修改主题背景
 #cp -f $GITHUB_WORKSPACE/argon/img/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
